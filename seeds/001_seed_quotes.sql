9082897652e217a068e48849c060468f52ce9c42
Sarah Tennis <sarah@Sarahs-Air.cable.rcn.com>
2024-11-18 19:05:24 -0600

    Organize seed data into characters dir for dynamic seeding, remove unneeded sql files, add UNIQUE constraint to a few attributes in migrations sql

--- seeds/001_seed_quotes.sql
+++
@@ -1,9 +0,0 @@
--- Slippy
-INSERT INTO quotes (content) VALUES ('Woah! Can you make it?');
-INSERT INTO quotes (content) VALUES ('Escaping? I don''t think so!');
--- Falco
-INSERT INTO quotes (content) VALUES ('Ah. You''re getting better, Fox.');
-INSERT INTO quotes (content) VALUES ('Back off, Fox.');
--- Pigma
-INSERT INTO quotes (content) VALUES ('Peppy, long time no see.');
-INSERT INTO quotes (content) VALUES ('I''ll do you fast, Peppy ol'' pal.');
